a92577d0f94a25f054694a23c5b9520b8358be3c
Eason.Y <easonyang2005@gmail.com>
2025-11-29 02:13:54 -0500

    Update build

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,13 @@
             This webpage was built using Vue.js<br>
             I did this by first installing node.js on my laptop. Now I have npm (Node Package Manager)<br>
             I then installed Vue.js with npm. After I build the webpage by setting up the app.vue, main.js and the index.html<br>
-            After I've created the webpage, I use npm to build and it's now avaible in a folder name dist.
+            After I've created the webpage, I use npm to build and it's now avaible in a folder name dist.<br>
+            Next I shared in on GitHub via npm, and host it on GitHub pages. I did have to transfer all files before I deploy it though<br>
+            Challenges:<br>
+            I encountered quite a lot of challenges when attempting this problem, first I didn't understand how I should use Vue.js<br>
+            Installing it was fine, however, it was pretty torturing to understand main.js, App.vue and index.html. I had to look at some videos to understand.<br>
+            Finally, after I've completed the code, hosting it was another challenge, I had to follow a tutorial step by step, yet still multiple issues occured
+             and took me a long time to figure out the solution. Fortunately, I successfully employed it on GitHub pages.  
         </p>
     </div>
 </template>
@@ -20,7 +26,8 @@
     }
 
     p{
+        width: 80vw;
         color: black;
-        font-size: 2.5vw;
+        font-size: 1.5vw;
     }
 </style>
